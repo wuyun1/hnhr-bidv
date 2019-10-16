94a382ef8d4e0a3d90dffd1347c096d0ec84f189
13485 <yunqiang.wu@hand-china.com>
2019-10-16 15:02:55 +0800

    update

--- .ci.sh
+++ .ci.sh
@@ -7,62 +7,6 @@ export CICD_EXECUTION_SEQUENCE=${BUILD_NUMBER:-1}
 docker build . -t  harbor.hft.jajabjbj.top:30088/hft/hnhr:${CICD_EXECUTION_SEQUENCE}
 docker push harbor.hft.jajabjbj.top:30088/hft/hnhr:${CICD_EXECUTION_SEQUENCE}
 
-# envsubst '${CICD_EXECUTION_SEQUENCE}' < deployment.yaml > _deployment.yaml
-
-cat > _deployment.yaml <<'EOF'
-kind: Service
-apiVersion: v1
-metadata:
-  name: hnhr
-  namespace: hzero
-spec:
-  selector:
-    app: hnhr
-  type: ClusterIP
-  ports:
-    - protocol: TCP
-      port: 8586
-      targetPort: 8586
----
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: hnhr
-  namespace: hzero
-  labels:
-    app: hnhr
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: hnhr
-  template:
-    metadata:
-      labels:
-        app: hnhr
-    spec:
-      imagePullSecrets:
-      - name: harbor
-      containers:
-      - name: hnhr
-        image: harbor.hft.jajabjbj.top:30088/hft/hnhr:${CICD_EXECUTION_SEQUENCE}
-        ports:
-        - containerPort: 8586
----
-apiVersion: extensions/v1beta1
-kind: Ingress
-metadata:
-  namespace: hzero
-  generation: 1
-  name: hnhr
-spec:
-  rules:
-  - host: hnhr.jd1.jajabjbj.top
-    http:
-      paths:
-      - backend:
-          serviceName: hnhr
-          servicePort: 8586
-EOF
+envsubst '${CICD_EXECUTION_SEQUENCE}' < deployment.yaml > _deployment.yaml
 
 kubectl apply -f _deployment.yaml
